a012e251903b8fbe330eff65cab1bed4f0bd2575
riccardobovinelli <44288679+riccardobovinelli@users.noreply.github.com>
2020-11-28 10:32:16 +0100

    Add files via upload

--- scripts/installwindscribe.sh
+++ scripts/installwindscribe.sh
@@ -1,5 +1,7 @@
+xrandr --output Virtual1 --mode 1024x768
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
 echo 'deb https://repo.windscribe.com/ubuntu zesty main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
 sudo apt-get update
 sudo apt-get install windscribe-cli
 windscribe login
+firefox https://windscribe.com/signup
